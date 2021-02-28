442fa4b01f53e43dcee40e84a746290671443c43
carol <carol@192.168.1.71>
2021-02-27 19:09:19 -0600

    Primer proyecto java

---
+++ src/primerproyectojava_matricula54786/PrimerProyectoJava_matricula54786.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyectojava_matricula54786;
+
+/**
+ *
+ * @author carol
+ */
+public class PrimerProyectoJava_matricula54786 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int y = 194832;
+        System.out.println("Int vale \n" + y);
+        
+        char num = 'a';
+        System.out.println("Num vale \n" + num);
+        
+        boolean x = true;
+        System.out.println("La variable vale \n" + x);
+        
+         
+    }
+   
+}
